7764b7ea9c04800fe32e0cb70e8faf01f1a1243d
Jinhyuk <pjh08190819@naver.com>
2019-03-15 02:51:18 +0900

    node study

--- node/main.js
+++ node/main.js
@@ -1,16 +1,41 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);   // 파라미터 id 값 출력
+    var title = queryData.id
+    if(_url == '/'){
+        title = 'Welcome';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(url);
-    response.end(fs.readFileSync(__dirname + url));
- 
+    fs.readFile(`data/${title}`, 'utf-8', function(err, description){
+        var template = `<!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="./?id=HTML">HTML</a></li>
+        <li><a href="./?id=CSS">CSS</a></li>
+        <li><a href="./?id=Javascript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      ${description}
+      </p>
+    </body>
+    </html>
+    `;
+    response.end(template);
+    });
+    // response.end(fs.readFileSync(__dirname + _url));
 });
 app.listen(3000);
